06a2998848589a06ac713540cab7ecfba5b4e6b5
3122251458 <99737935+3122251458@users.noreply.github.com>
2022-05-04 21:30:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <button type="button" class="btn btn-secondary btn-lg" id="replay" onclick="replayBtn()">重来</button>
         <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.reload()">主页</button>
 			<button type="button" class="btn btn-secondary btn-lg" onclick="goRank();">排行</button>
-            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://qm.qq.com/cgi-bin/qm/qr?k=GVf6wv9STYGgw66IdYUL75TJiapu1_CT&noverify=0'">联系作者</button>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://qm.qq.com/cgi-bin/qm/qr?k=2BxIarndIl85XdYJBxaq77ME7j58scka&noverify=0'">联系作者</button>
         </div>
     </div>
     </div>
